ba3b718f72c3c442517e5977df9d0f49af542bba
kirill sychev <kirill>
2022-08-27 11:46:52 +0500

    новые импорты

--- src/components/index.js
+++
@@ -1,5 +0,0 @@
-import Main from "./main/Main"
-import Menu from "./menu/Menu"
-import About from "./main/About"
-
-export { Menu, Main, About }
---
+++ src/screens/index.js
@@ -0,0 +1,5 @@
+import Skills from "./skills/Skills"
+import Main from "./main/Main"
+import About from "./about/About"
+
+export { Skills, Main, About }
